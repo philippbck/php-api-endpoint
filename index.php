078adcd9827a4ebffc2b0791d4e0421e90bd5418
Philipp Buck <p.buck.contact@gmail.com>
2019-05-03 23:24:41 +0200

    fix

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 
 <?php
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Authorization, Access, Origin, Content-Type, X-Auth-Token');
+//header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//header('Access-Control-Allow-Headers: Authorization, Access, Origin, Content-Type, X-Auth-Token');
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
